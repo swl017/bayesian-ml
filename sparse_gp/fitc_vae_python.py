f3db2e76b13c0c70a1fa5e83efd0bd1ac6f7c26d
Seungwook <seungwook1024@gmail.com>
2020-10-19 15:35:52 +0900

    sparse_gp initial commit

---
+++ sparse_gp/fitc_vae_python.py
@@ -0,0 +1,35 @@
+# https://bwengals.github.io/fitc-and-vfe.html
+#
+import pymc3 as pm
+
+import theano
+import theano.tensor as tt
+import numpy as np
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+np.random.seed(10)
+
+nx, nu = 60, 7
+x = np.random.rand(nx)
+xu = np.random.rand(nu)
+y = np.sin(2 * np.pi * x * 2.5) + 0.3 * np.random.randn(nx)
+
+plt.plot(x, y, 'ko', ms=2);
+plt.plot(xu, 0.0*np.ones(len(xu)), 'rx', ms=10)
+x = x[:,None]; xu = xu[:,None]
+xs = np.linspace(-0.1, 1.1, 100)
+
+"""
+Full GP
+"""
+
+with pm.Model() as model:
+    ℓ = pm.Gamma("ℓ", alpha=1.5, beta=0.5)
+    σ_f = pm.HalfCauchy("σ_f", beta=5)
+    σ_n = pm.HalfCauchy("σ_n", beta=5)
+    cov = tt.square(σ_f) * pm.gp.cov.ExpQuad(1, ℓ)
+    gp = pm.gp.GP("gp", x, cov, sigma=σ_n, observed=y)
+    trace = pm.sample(1000)
+
+    
